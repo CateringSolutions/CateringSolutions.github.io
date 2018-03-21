c3ec927711e8f183c43f04678c425d6e5551cabe
sgilbo618 <sgilbo618@gmail.com>
2018-03-21 11:12:24 -0400

    add breakfast menu and nav highlighting

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,9 +16,11 @@ $(document).ready(function(){
         autoplaySpeed: 5000,
         fade: true,
         speed: 1000
-        // prevArrow:'<div class="a-left control-c prev slick-prev"><span class="fas fa-chevron-left"></span><span class="sr-only">Prev</span></div>',
-        // nextArrow:'<div class="a-right control-c next slick-next"><span class="fas fa-chevron-right"></span><span class="sr-only">Next</span></div>'
     });
     var year = new Date().getFullYear();
     $("#year").text(year);
+    // toggle menu arrow icons
+    $(".menu-titles").on("click", function(){
+        $(this).find(".arrows").toggleClass("hidden");
+    });
 });
